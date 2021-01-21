81d6870ec2b4199173a91c195b767195d8aa4b41
tinkai <fllarduino6@gmail.com>
2021-01-21 15:22:17 +0900

    初めてのcommit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Kitty{
+  public:
+    Kitty();
+}obj;
+
+Kitty::Kitty(){
+    cout<<"Kitty on your lap\n";
+}
+
+int main(void){
+    // Your code here!
+  Kitty obj;
+  return 0;
+}
